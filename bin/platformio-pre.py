1834b203d5edf4ae7659f3415b9ef407ba22fd42
Austin Lane <vidplace7@gmail.com>
2025-12-11 19:09:07 -0500

    Optimize builds to reduce duplicate dependency checks
    'mtjson' will now build all required pieces when they don't exist

--- bin/platformio-pre.py
+++ bin/platformio-pre.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python3
 # trunk-ignore-all(ruff/F821)
 # trunk-ignore-all(flake8/F821): For SConstruct imports
+from readprops import readProps
 Import("env")
 platform = env.PioPlatform()
+prefsLoc = env["PROJECT_DIR"] + "/version.properties"
+verObj = readProps(prefsLoc)
 
 if platform.name == "native":
     env.Replace(PROGNAME="meshtasticd")
 else:
-    from readprops import readProps
-    prefsLoc = env["PROJECT_DIR"] + "/version.properties"
-    verObj = readProps(prefsLoc)
     env.Replace(PROGNAME=f"firmware-{env.get('PIOENV')}-{verObj['long']}")
-
-# Print the new program name for verification
-print(f"PROGNAME: {env.get('PROGNAME')}")
+    print(f"PROGNAME: {env.get('PROGNAME')}")
+    if platform.name == "espressif32":
+        env.Replace(ESP32_FS_IMAGE_NAME=f"littlefs-{env.get('PIOENV')}-{verObj['long']}")
+        print(f"ESP32_FS_IMAGE_NAME: {env.get('ESP32_FS_IMAGE_NAME')}")
+    elif platform.name == "raspberrypi":
+        env.Replace(PICO_FS_IMAGE_NAME=f"littlefs-{env.get('PIOENV')}-{verObj['long']}")
+        print(f"PICO_FS_IMAGE_NAME: {env.get('PICO_FS_IMAGE_NAME')}")
